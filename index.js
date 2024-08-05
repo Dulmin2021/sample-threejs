d2c055e18fede892bc5ca62b21bec87db09d3d78
Dulmin2021 <dulmin.edu@gmail.com>
2024-08-05 23:15:29 +0530

    updated code

--- index.js
+++ index.js
@@ -16,14 +16,27 @@ camera.position.z =2;
 const scene = new THREE.Scene();
 
 const geo = new THREE.IcosahedronGeometry(1.0, 2);
-const mat = new THREE.MeshBasicMaterial({
-    color:0xccff
+const mat = new THREE.MeshStandardMaterial({
+    color:0xffff,
+    flatShading:true
 });
-const mesh =new THREE.Mesh(geo, mat);
+const mesh =new THREE.Mesh(geo, mat); 
 scene.add(mesh);    
 
+const wireMat =new THREE.MeshBasicMaterial({
+    color:0xffffff,
+    wireframe:true
+});
+const wireMesh =new THREE.Mesh(geo,wireMat);
+mesh.add(wireMesh);
+
+const hemiLight = new THREE.HemisphereLight(0xffffff,0x000000)
+scene.add(hemiLight);
+
 function animate(t=0){
+    console.log(t);
     requestAnimationFrame(animate);
+    mesh.rotation.y =t =0.0001;
     mesh.scale.setScalar(Math.cos(t*0.001)+1.0);
     renderer.render(scene, camera);
 }
